b40ed96af41cbf31e8d29ef1620bf474f836892c
plopez062500 <plopez062500@gmail.com>
2023-10-07 02:38:32 -0500

    added readonly, extendable_right, and extendable_left props

--- src/lib/controls/Input.svelte
+++ src/lib/controls/Input.svelte
@@ -12,14 +12,17 @@
 <script lang="ts">
 	export let full_width: boolean = false;
 	export let size: Size = 'md';
+	export let extendable_right: boolean = false;
+	export let extendable_left: boolean = false;
 
-	export let type: InputType = 'password';
+	export let type: InputType = 'text';
 	export let name: string;
 	export let label: string = name;
 	export let placeholder: string = name;
 	export let value: string = '';
 	export let min: number = Number.MIN_SAFE_INTEGER;
 	export let max: number = Number.MAX_SAFE_INTEGER;
+	export let readonly: boolean = false;
 </script>
 
 <label>
@@ -28,8 +31,11 @@
 		<input
 			class={`${sizes[size]} border border-gray-300 rounded-sm outline-blue-300  transition-all block`}
 			class:w-full={full_width}
+			class:border-r-0={extendable_right}
+			class:border-l-0={extendable_left}
 			{name}
 			{placeholder}
+			{readonly}
 			bind:value
 		/>
 	{/if}
@@ -38,9 +44,12 @@
 		<input
 			class={`${sizes[size]} border border-gray-300 rounded-sm outline-blue-300  transition-all block`}
 			class:w-full={full_width}
+			class:border-r-0={extendable_right}
+			class:border-l-0={extendable_left}
 			type='password'
 			{name}
 			{placeholder}
+			{readonly}
 			bind:value
 		/>
 	{/if}
@@ -49,9 +58,12 @@
 		<input
 			class={`${sizes[size]} border border-gray-300 rounded-sm outline-blue-300  transition-all block`}
 			class:w-full={full_width}
+			class:border-r-0={extendable_right}
+			class:border-l-0={extendable_left}
 			type="number"
 			{name}
 			{placeholder}
+			{readonly}
 			{min}
 			{max}
 			bind:value
